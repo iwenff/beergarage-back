921affd90661c79e88702ebab5c82fb649d49f8d
whqr <whqr@MacBookNikita.local>
2026-06-12 12:23:30 +0300

    fix: update CORS for new domain beer-garage.vercel.app

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://bear-garage-landing.vercel.app', 'http://localhost:3000'],
+    origin: [
+      'https://beer-garage.vercel.app',
+      'http://localhost:5173',
+      'http://localhost:3000',
+    ],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
   });
